a808997007afe29803cc71cfb06993ec615d80ef
alena.hodchenkova <alena.hodchenkova@ab-soft.net>
2018-03-12 13:12:41 +0200

    fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,46 @@
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
 
 
     public static String reverseWithCharArray(String inputString) {
         char[] charArray = inputString.toCharArray();
         String resultString = "";
         for (int i = charArray.length - 1; i >= 0; i--) {
-            resultString += charArray[i];
+            resultString = resultString + charArray[i];
         }
         return resultString;
     }
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        String myString = "AB Soft";
+//        String[] subStr;
+//        String delimeter = " ";
+//        subStr = myString.split(delimeter);
+//        for(int i = 0; i < subStr.length; i++) {
+//            String word1 = subStr[0];
+//            String word2 = subStr[1];
+//            System.out.println("AB Soft reverse :" + reverseWithCharArray(word1) + " " + (word2));
+//        }
+//
+//    }
 
+//}
+    public static void main(String[] args) {
         String myString = "AB Soft";
-        String[] subStr;
         String delimeter = " ";
-        subStr = myString.split(delimeter);
-        for(int i = 0; i < subStr.length; i++) {
-            String word1 = subStr[0];
-            String word2 = subStr[1];
-            System.out.println("AB Soft reverse :" + reverseWithCharArray(word1) + " " + (word2));
+        String[] subStr = myString.split(delimeter);
+        System.out.print(myString + " reverse :");
+        for (String substr : subStr) {
+            System.out.print(reverseWithCharArray(substr));
+            System.out.print(" ");
         }
 
-    }
-}
+
+
+}}
+
 
 
 
